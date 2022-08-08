cc55d21d760276f1bd2d8e344d59dbbd31b09796
Danieli-Costa <danielibibiano2016@gmail.com>
2022-08-08 14:49:50 -0700

    Changes on css, html and javascript

--- src/javascript.js
+++ src/javascript.js
@@ -51,21 +51,9 @@ function search(city) {
 function searchCity(event) {
   event.preventDefault();
   debugger;
-  let city = document.querySelector("#location").value;
+  let city = document.querySelector(".form-control").value;
   search(city);
 }
-
-function searchLocation(position) {
-  let apiKey = "b0fe589cc24a909e1d262c962ba031e6";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemperature);
-}
-function currentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", currentLocation);
 
 let formSearch = document.querySelector(".search-form");
 formSearch.addEventListener("submit", searchCity);
